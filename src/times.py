93a984cfe262003fc84d29e80c438f19eea074cb
superlion3 <melvinchen610@gmail.com>
2023-11-08 06:46:17 -0500

    fixed get_timezone_offset

--- src/times.py
+++ src/times.py
@@ -2,14 +2,14 @@ from datetime import time, datetime, timedelta
 from dateutil import tz
 
 est = tz.gettz('America/New_York')
-dst = datetime.now(est)
 
 midnight = time(0, 0, 0, 0, est)
 eight_am = time(8, 0, 0, 0, est)
 nine_pm = time(21, 0, 0, 0, est)
 
-def get_utc_offset(__dt: datetime) -> int:
-    """Get the UTC offset of the current time in hours."""
+def get_utc_offset(__tz = est) -> int:
+    """Get the number of hours that UTC is ahead of the given timezone."""
     
-    now = datetime.now(est)
-    return est.utcoffset(now).total_seconds() // 3600
+    now = datetime.now(__tz)
+    offset = __tz.utcoffset(now).total_seconds() // 3600
+    return -int(offset)
