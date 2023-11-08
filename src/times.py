83af91ab78ec353d689f158fe5e1f77f7835f57c
superlion3 <melvinchen610@gmail.com>
2023-11-07 22:58:54 -0500

    fixed timezone error

--- src/times.py
+++ src/times.py
@@ -1,10 +1,10 @@
-from datetime import time, timedelta, timezone
+from datetime import time
+from dateutil import tz
 
-est = timezone(timedelta(hours=-4))
+est = tz.gettz('America/New_York')
 midnight = time(0, 0, 0, 0, est)
 before_midnight = time(23, 59, 30, 0, est)
 nine_pm = time(21, 0, 0, 0, est)
 before_nine_pm = time(20, 59, 30, 0, est)
 eight_am = time(8, 0, 0, 0, est)
 before_eight_am = time(7, 59, 30, 0, est)
-
